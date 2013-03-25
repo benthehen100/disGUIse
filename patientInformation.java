8f56fa0e4e686fe754dcccafdd0aa6929db46d6f
Michael Gilardi <mdgilardi@gmail.com>
2013-03-25 16:10:59 -0700

    Test commit

---
+++ patientInformation.java
@@ -0,0 +1,30 @@
+
+public class patientInformation {
+
+	String name, address, insurance, phone, height, weight;
+	
+	public void SavePhone(String newPhone) {
+		phone = newPhone;
+	}
+	
+	public void SaveAddress(String newAddress) {
+		address = newAddress;
+	}
+	
+	public void SaveName(String newName) {
+		name = newName;
+	}
+	
+	public void SaveInsurance(String newInsurance) {
+		insurance = newInsurance;
+	}
+	
+	public void SaveHeight(String newHeight) {
+		height = newHeight;
+	}
+	
+	public void SaveWeight(String newWeight) {
+		weight = newWeight;
+	}
+	
+}
