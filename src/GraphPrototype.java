7145043d0b2dba5c32dd49ef95926ce098a897cf
Stoze <stoze1337@gmail.com>
2013-04-22 00:42:46 -0700

    GraphProtoype is my try at a graph, ReadIndicators reads the file graphdata and puts the indicators into different arrayLists. Mainly need to figure out how to get the indicators from the readfile and apply one of them to the graph. From there, figure out how to put 3 graphs into one component, add some axis labels, and then integrate them into the updateIndicators pane.

---
+++ src/GraphPrototype.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.*;
+ 
+
+
+public class GraphPrototype{
+	public static void main(String[] args) {
+		
+        JFrame Frame = new JFrame();
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Frame.getContentPane().add(new GraphED()); 
+        Frame.setSize(500,500); 
+        Frame.setVisible(true);
+    }
+}
+
+ class GraphED extends JPanel {
+    int[] bldpressure = { 0, 99, 80, 21, 120, 32};                               //this is the data run through the graph
+    int scalefact = 20;                                                          //zoom of graph/scaling factor
+ 
+    protected void paintComponent(Graphics g) {
+    	int width = getWidth();      //width of component
+        int height = getHeight();    //height of component
+        
+        super.paintComponent(g);
+        Graphics2D Graph = (Graphics2D)g;                                                               //creates paint component
+        Graph.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);     //makes lines cleaner
+        Graph.draw(new Line2D.Double(scalefact, scalefact, scalefact, height-scalefact));               //y-axis black line
+
+        Graph.draw(new Line2D.Double(scalefact, height-scalefact, width-scalefact, height-scalefact)); //x-axis black line
+   
+        
+        double xScale = (double)(width - 2*scalefact)/(bldpressure.length-1);      //set zoom
+        double yScale = (double)(height - 2*scalefact)/getMax();
+        Graph.setPaint(Color.blue.darker());                                      //color of line
+        for(int i = 0; i < bldpressure.length-1; i++) {
+            double xcoor1 = scalefact + i*xScale;                                  //set x coordinate first point
+            double ycoor1 = height - scalefact - yScale*bldpressure[i];            //set y coordinate of first point
+            double xcoor2 = scalefact + (i+1)*xScale;                              //set x coor of second point
+            double ycoor2 = height - scalefact - yScale*bldpressure[i+1];          //set y coor of second point
+            Graph.draw(new Line2D.Double(xcoor1, ycoor1, xcoor2, ycoor2));         //draw a line between those points
+        }
+        
+        Graph.setPaint(Color.blue);                                       //color of data points
+        for(int i = 0; i < bldpressure.length; i++) {
+            double x = scalefact + i*xScale;                            //where x is based on the line
+            double y = height - scalefact - yScale*bldpressure[i];     //where y is based on the line
+            Graph.fill(new Ellipse2D.Double(x-2, y-2, 4, 4));         //draw circle/eclipse at points on line
+        }
+    }
+ 
+     private int getMax() {                                         //determines max to see how high to make graph
+        int val_max = 0;
+        for(int i = 0; i < bldpressure.length; i++) {
+            if(bldpressure[i] > val_max)
+                val_max = bldpressure[i];
+        }
+        return val_max;
+    } 
+ 
+}
---
+++ src/ReadIndicators.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+ 
+	
+	
+public class ReadIndicators {
+	
+	 
+	public static void main(String[] args) {
+ 
+		ArrayList<Integer> data = new ArrayList<Integer>();             //So the arrayList data gets all the integers in the file,
+		ArrayList<Integer> bldpressure = new ArrayList<Integer>();      //and then splits them into different arrayLists for the
+		ArrayList<Integer> bldsugar = new ArrayList<Integer>();         //indicators.
+		ArrayList<Integer> weight = new ArrayList<Integer>();
+		Scanner fileScanner = null;
+		try {
+				fileScanner = new Scanner(new File("graphdata"));
+		    } 
+		    catch (FileNotFoundException e) 
+		    {
+		    	e.printStackTrace();
+		    }
+		while (fileScanner.hasNextInt())
+		{
+		   data.add(fileScanner.nextInt());
+		}
+		
+		/*for(int i=0; i<data.size(); i++)           //test to see if arrayList gets values
+		{
+        System.out.println(data.get(i));
+		}*/
+		
+		for(int i=0; i<data.size(); i=i+3)           //takes data, seperates it into three different
+		{                                            //arrayLists for the 3 indicators.
+			bldpressure.add(data.get(i));
+			bldsugar.add(data.get(i+1));
+			weight.add(data.get(i+2));
+		}
+		
+		for (int i =0; i<bldpressure.size(); i++)   //this outputs the three arrayLists, shows that
+		{                                           //they are getting correct values.
+			System.out.println(bldpressure.get(i));
+		}
+		
+		System.out.println();
+		
+		for (int i =0; i<bldsugar.size(); i++)
+		{
+			System.out.println(bldsugar.get(i));
+		}
+		
+		System.out.println();
+		
+		for (int i =0; i<weight.size(); i++)
+		{
+			System.out.println(weight.get(i));
+		}
+		
+		
+		}
+	}
+
