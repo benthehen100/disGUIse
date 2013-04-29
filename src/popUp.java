ad944ee0e8897bf32764dd09abcae5edd03ac941
Weeam Awad <Weeam Awad@192.168.1.142>
2013-04-28 20:10:40 -0700

    added comments

--- src/popUp.java
+++ src/popUp.java
@@ -2,22 +2,19 @@ import javax.swing.JDialog;
 import java.awt.Toolkit;
 
 
-public class popUp extends JDialog{
+public class popUp extends JDialog{ //creates popUP class which extends JDialog box
 	
 	public popUp() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(popUp.class.getResource("/disGUIse_images/new_add_plus_red.png")));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(popUp.class.getResource("/disGUIse_images/new_add_plus_red.png"))); //sets the icon of the frame
 		setTitle("disGUIse Well-Check Clinic");
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setModal(true);
-	
-		
-		
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE); //exits on close
+		setModal(true); //makes so other windows cant be chosen when this is open
 	}
 	public static void main(String[] args) {
 		
-		popUp dialog = new popUp();
+		popUp dialog = new popUp(); //creates new dialog
 		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		dialog.setVisible(true);
+		dialog.setVisible(true); //visible is ture
 		
 }
 
